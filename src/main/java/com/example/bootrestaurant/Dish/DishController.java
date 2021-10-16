89ebf9a8cda4f853c7fbf2b208e81e626e4714c0
kunict11 <tanja.kunic@gmail.com>
2021-10-16 20:58:45 +0200

    Implement finding dishes by id

--- src/main/java/com/example/bootrestaurant/Dish/DishController.java
+++ src/main/java/com/example/bootrestaurant/Dish/DishController.java
@@ -3,6 +3,7 @@ package com.example.bootrestaurant.Dish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
@@ -20,14 +21,17 @@ public class DishController {
     }
 
     @GetMapping("/dishes")
-    public Map<DishType, List<Dish>> getAllDishes() {
+    Map<DishType, List<Dish>> getAllDishes() {
         List<Dish> dishes = new ArrayList<>();
         dishRepo.findAll().forEach(dishes::add);
-        System.out.println(findAllGrouped(dishes));
+
         return findAllGrouped(dishes);
     }
 
-
+    @GetMapping("/dishes/{id}")
+    Optional<Dish> getDishById(@PathVariable Integer id) {
+        return dishRepo.findById(id);
+    }
     private Map<DishType, List<Dish>> findAllGrouped(List<Dish> dishes) {
         return dishes.stream().collect(Collectors.groupingBy(Dish::getDishType));
     }
